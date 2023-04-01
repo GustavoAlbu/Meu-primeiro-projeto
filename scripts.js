8417e7ce280b3f1ff81ccce6ff50f3dda6d486a3
GustavoAlbu <gustavoalbudev@gmail.com>
2023-03-31 23:50:50 -0300

    Primeiro Commit-Projeto responsividade

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+
+function trocaimagem(imagem){
+    let imgiphone = document.querySelector(".Iphone-1")
+    imgiphone.src = imagem
+
+
+
+
+}
